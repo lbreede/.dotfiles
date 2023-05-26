ab2f339bd4c38a35c44f077d7d3caa94b3f2930f
lbreede <lennart.breede@gmail.com>
2023-05-25 21:50:33 -0700

    modified 2023-05-28T21:50

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,7 @@
 require("remap")
+require("set")
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
- 
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-
-vim.g.mapleader = ","
+vim.g.mapleader = " "
