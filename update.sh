9933ad3d7964ba421fc4b051ead76bdfaa23d277
lbreede <lennart.breede@gmail.com>
2023-05-25 17:18:55 -0700

    changed update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+rm .zshenv
+cp ~/.zshenv .
+
 rm .zshrc
 cp ~/.zshrc .
 
 rm -r .config
+
+# consider using below if .zshrc lives in .config/zsh/
+# mkdir -p .config/zsh
+# cp ~/.config/zsh/.zshrc .config/zsh
 
 mkdir -p .config/nvim
 cp ~/.config/nvim/init.lua .config/nvim
