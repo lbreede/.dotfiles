16075784833a65a7c37fe41af8e25cce2d821ff8
lbreede <lennart.breede@gmail.com>
2023-05-25 17:19:20 -0700

    changed nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
-require("lbreede")
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- map("n", "{", "{zz", {noremap = true, silent = false})
+-- map("n", "}", "}zz", {noremap = true, silent = false})
